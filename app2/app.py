2a934eaba96c646d9e2002bd7945c44490b5912b
torodriguezt <torodriguezt@unal.edu.co>
2024-11-11 12:04:52 -0500

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -11,4 +11,4 @@ def static_files(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8082)
